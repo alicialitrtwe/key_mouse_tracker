cca723f156fccb0d022245da3543c2802c4e75ef
Yijia-Chen <yijiachenmain@gmail.com>
2021-11-03 14:50:09 -0700

    fix: locks on press and release methods to prevent data race

--- key_tracker_private.py
+++ key_tracker_private.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import numpy as np
 from itertools import chain
 import os
+import threading
 
 class PrivateKeyTracker:
     """
@@ -25,13 +26,15 @@ class PrivateKeyTracker:
         The file of designated log output, opened with overwrite permission
     summary_file : TextIOWrapper
         The file of designated summary output, opened with overwrite permission
+    lock : threading.Lock
+        The lock which prevents press and release actions from interleaving
     is_last_action_release : bool
         Whether the last action is release or not
     last_pressed_key : pynput.keyboard.Key
         The last key pressed
     last_pressed_time : float
         The timestamp at which the last key was pressed at any given time 
-        during session
+during session
     key_press_spans : dict[str, list[float]]
         The key press spans of corresponding types of keys
 
@@ -48,13 +51,13 @@ class PrivateKeyTracker:
             os.mkdir(output_dir)
 
         self.start_time = time.time()
-
         self.start_datetime = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
         log_filename = 'log_' + self.start_datetime
         summary_filename = 'summary_' + self.start_datetime
         self.log_file = open('outputs/' + log_filename, 'w+')
         self.summary_file = open('outputs/' + summary_filename, 'w+')
         
+        self.lock = threading.Lock()
         self.is_last_action_release = True
         self.last_pressed_time: dict[Key, float] = {}
         self.key_press_spans: dict[str, list[float]] = {
@@ -74,27 +77,34 @@ class PrivateKeyTracker:
             The key pressed
         """
 
-        # Only update last_pressed_time[key] if following a release action or if
-        # last key pressed is not the current key pressed. In other words, in 
-        # the event where the current key was pressed last and not released, do 
-        # not update its last pressed time value and instead count it as a 
-        # continued key press. 
-        if self.is_last_action_release or self.last_pressed_key != key:
-            self.last_pressed_time[key] = time.time()
-        self.last_pressed_key = key
+        self.lock.acquire() # making sure ongoing press and release actions complete
 
         try:
-            # Do NOT output this value, in order to preserve user privacy
-            _x = key.char    
-            print('alphanumeric key pressed')
-        except AttributeError:
-            if key == Key.backspace:
-                print('backspace key pressed')
-            elif key == Key.delete:
-                print('delete key pressed')
-            else:
-                print('special key pressed')
-        self.is_last_action_release = False
+            # Only update last_pressed_time[key] if following a release action or if
+            # last key pressed is not the current key pressed. In other words, in 
+            # the event where the current key was pressed last and not released, do 
+            # not update its last pressed time value and instead count it as a 
+            # continued key press. 
+            if self.is_last_action_release or self.last_pressed_key != key:
+                self.last_pressed_time[key] = time.time()
+            self.last_pressed_key = key
+
+            try:
+                # Do NOT output this value, in order to preserve user privacy
+                _x = key.char    
+                print('alphanumeric key pressed')
+            except AttributeError:
+                if key == Key.backspace:
+                    print('backspace key pressed')
+                elif key == Key.delete:
+                    print('delete key pressed')
+                else:
+                    print('special key pressed')
+
+            self.is_last_action_release = False
+
+        finally:
+            self.lock.release()
 
     def __on_release(self, key: Key):
         """
@@ -106,65 +116,64 @@ class PrivateKeyTracker:
             The key released
         """
 
-        if key in self.last_pressed_time:
-            key_press_span = time.time() - self.last_pressed_time[key]
-        else:
-            # Occurs when typing very fast and previous release processing is 
-            # not yet finished
-            key_press_span = 0
-
+        self.lock.acquire() # making sure ongoing press and release actions complete
         try:
-            # Do NOT output this value, in order to preserve user privacy
-            _x = key.char    
-            print('alphanumeric key released')
-            self.key_press_spans['alphanumeric'].append(key_press_span)
-            self.log_file.write('some alphanumeric, %f\n' % key_press_span)
-        except AttributeError:
-            if key == Key.backspace:
-                print('backspace key released')
-                self.key_press_spans['backspace'].append(key_press_span)
-                self.log_file.write('backspace, %f\n' % key_press_span)
-            elif key == Key.delete:
-                print('delete key released')
-                self.key_press_spans['delete'].append(key_press_span)
-                self.log_file.write('delete, %f\n' % key_press_span)
-            else:
-                print('special key released')
-                self.key_press_spans['other special'].append(key_press_span)
-                self.log_file.write('some other special, %f\n' % key_press_span)
-        
-        self.is_last_action_release = True
+            key_press_span = time.time() - self.last_pressed_time[key]
 
-        if key == Key.esc:
-            # close log file
-            self.log_file.close()
+            try:
+                # Do NOT output this value, in order to preserve user privacy
+                _x = key.char    
+                print('alphanumeric key released')
+                self.key_press_spans['alphanumeric'].append(key_press_span)
+                self.log_file.write('some alphanumeric, %f\n' % key_press_span)
+            except AttributeError:
+                if key == Key.backspace:
+                    print('backspace key released')
+                    self.key_press_spans['backspace'].append(key_press_span)
+                    self.log_file.write('backspace, %f\n' % key_press_span)
+                elif key == Key.delete:
+                    print('delete key released')
+                    self.key_press_spans['delete'].append(key_press_span)
+                    self.log_file.write('delete, %f\n' % key_press_span)
+                else:
+                    print('special key released')
+                    self.key_press_spans['other special'].append(key_press_span)
+                    self.log_file.write('some other special, %f\n' % key_press_span)
+            
+            self.is_last_action_release = True
 
-            self.end_time = time.time()
-            count_alphanumeric = len(self.key_press_spans['alphanumeric'])
-            count_backspace = len(self.key_press_spans['backspace'])
-            count_delete = len(self.key_press_spans['delete'])
-            count_other_special = len(self.key_press_spans['other special'])
-            count_total = count_alphanumeric + count_backspace + count_delete + count_other_special
-            ratio_error_to_total = (count_backspace + count_delete) / count_total
+            if key == Key.esc:
+                # close log file
+                self.log_file.close()
 
-            # join press spans for all types of keys, then compute average
-            mean_key_press_span: float = np.average(list(chain.from_iterable(self.key_press_spans.values())))
+                self.end_time = time.time()
+                count_alphanumeric = len(self.key_press_spans['alphanumeric'])
+                count_backspace = len(self.key_press_spans['backspace'])
+                count_delete = len(self.key_press_spans['delete'])
+                count_other_special = len(self.key_press_spans['other special'])
+                count_total = count_alphanumeric + count_backspace + count_delete + count_other_special
+                ratio_error_to_total = (count_backspace + count_delete) / count_total
 
-            # write summary of session
-            self.summary_file.write('session started at %s\n' % self.start_datetime)
-            self.summary_file.write('session length is %f seconds\n' % (self.end_time - self.start_time))
-            self.summary_file.write('alphanumeric keys pressed %d times\n' % count_alphanumeric)
-            self.summary_file.write('backspace key pressed %d times\n' % count_backspace)
-            self.summary_file.write('delete key pressed %d times\n' % count_delete)
-            self.summary_file.write('other special keys pressed %d times\n' % count_other_special)
-            self.summary_file.write('total keys pressed %d times\n' % count_total)
-            self.summary_file.write('error to total ratio is %f\n' % ratio_error_to_total)
-            self.summary_file.write('average key press span is %f seconds\n' % mean_key_press_span)
+                # join press spans for all types of keys, then compute average
+                mean_key_press_span: float = np.average(list(chain.from_iterable(self.key_press_spans.values())))
 
-            self.summary_file.close()
+                # write summary of session
+                self.summary_file.write('session started at %s\n' % self.start_datetime)
+                self.summary_file.write('session length is %f seconds\n' % (self.end_time - self.start_time))
+                self.summary_file.write('alphanumeric keys pressed %d times\n' % count_alphanumeric)
+                self.summary_file.write('backspace key pressed %d times\n' % count_backspace)
+                self.summary_file.write('delete key pressed %d times\n' % count_delete)
+                self.summary_file.write('other special keys pressed %d times\n' % count_other_special)
+                self.summary_file.write('total keys pressed %d times\n' % count_total)
+                self.summary_file.write('error to total ratio is %f\n' % ratio_error_to_total)
+                self.summary_file.write('average key press span is %f seconds\n' % mean_key_press_span)
 
-            # stop listener
-            return False
+                self.summary_file.close()
+
+                # stop listener
+                return False
+        finally:
+            self.lock.release()
     
     def start(self):
         """
