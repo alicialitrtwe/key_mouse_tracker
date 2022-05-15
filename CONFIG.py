27f05db7720030a6775550acc9c0bb5f7e5b2ad9
aliciaxz <aliciaxiaozeng@gmail.com>
2022-05-14 23:48:51 -0700

    feat: base class for trackers

--- CONFIG.py
+++ CONFIG.py
@@ -2,4 +2,4 @@
 SESSION_LENGTH_IN_HOURS = 1 / 120 # set to be 30 sec for debugging; change to 1 hr when tracking
 
 # Directory to save the outputs
-SAVE_DIR = '/Users/Alicia/PycharmProjects/key_tracker_private/save_dir'
+SAVE_DIR = '/Users/Alicia/PycharmProjects/key_mouse_tracker/save_dir'
--- KeyTracker.py
+++ KeyTracker.py
@@ -9,10 +9,9 @@ import CONFIG
 import KEY_DICT
 
 
-class KeyTrackerPrivate:
+class TrackerBase:
     """
-    A key tracker which identifies 'backspace' and 'delete' keys and groups
-    other keys into 'left_alphanum', 'right_alphanum', or 'other special' keys. The tracker outputs
+    A base class for key tracker and mouse tracker. Not to be used alone. The tracker outputs
     the log and meta info of tracking sessions in a directory named 'outputs'. The logs are stored
     in sub-folders for different dates
     ...
@@ -34,53 +33,47 @@ class KeyTrackerPrivate:
         The file of designated meta info output, opened with overwrite permission
     _lock : threading.Lock
         The lock which prevents press and release actions from interleaving
-    _is_last_action_release : bool
-        Whether the last action is release or not
-    _last_pressed_key : pynput.keyboard.Key
-        The last key pressed
-    _last_pressed_time : float
-        The timestamp at which the last key was pressed at any given time
-        during session
+    listener:
+        listener for mouse or keyboard
 
     Methods
     -------
     start()
-        Starts the tracker, which can be terminated by keyboard interrupt
+        Starts the tracker
+    stop()
+        Stops the tracker
     cron_new_session()
         End current session and start a new one. To be used as a cron job.
     """
 
     def __init__(self):
         # create output directory
-        self.dev = 'key'
+        self.dev = None
         self.listener = None
         self._meta_file = None
+        self.stopped = None
         self._lock = threading.Lock()
 
-        self.listener = keyboard.Listener(
-            on_press=self._on_press,
-            on_release=self._on_release)
-
         if CONFIG.SAVE_DIR is not None:
-            save_dir = CONFIG.SAVE_DIR
+            self.save_dir = CONFIG.SAVE_DIR
         else:
-            save_dir = os.getcwd()
-        self.output_dir = os.path.join(save_dir, f'{self.dev}/outputs')
-        self.metadata_dir = os.path.join(save_dir, f'{self.dev}/metadata')
-        if not os.path.exists(save_dir):
-            os.mkdir(save_dir)
-        if not os.path.exists(os.path.join(save_dir, self.dev)):
-            os.mkdir(os.path.join(save_dir, self.dev))
+            self.save_dir = os.getcwd()
+
+    def _get_paths(self):
+        self.output_dir = os.path.join(self.save_dir, f'{self.dev}/outputs')
+        self.metadata_dir = os.path.join(self.save_dir, f'{self.dev}/metadata')
+        if not os.path.exists(os.path.join(self.save_dir, self.dev)):
+            os.makedirs(os.path.join(self.save_dir, self.dev), exist_ok=False)
         if not os.path.exists(self.output_dir):
             os.mkdir(self.output_dir)
         if not os.path.exists(self.metadata_dir):
             os.mkdir(self.metadata_dir)
 
-
     def _start_session(self):
         """
-        Start a new session with appropriate attribute values.
+        Start a new session for logging: create new log file, write the column names
         """
+
         self.start_time = time.time()
         self.start_datetime = datetime.fromtimestamp(self.start_time).strftime('%Y-%m-%d_%H-%M-%S')
         self.start_date = self.start_datetime.split('_')[0]
@@ -89,13 +82,16 @@ class KeyTrackerPrivate:
             os.mkdir(os.path.join(self.output_dir, self.start_date))
         log_file_path = os.path.join(self.output_dir, self.start_date, f'{self.dev}_logger_{self.start_datetime}.csv')
         self._log_file = open(log_file_path, 'w+')
-        self._log_file.write('key_type, key_name, press_time, press_duration\n')
-        self._is_last_action_release = True
-        self._last_pressed_time: Dict[Key, float] = {}
+        if self.dev == 'key':
+            self._log_file.write('key_type, key_name, timestamp, duration\n')
+            self._is_last_action_release = True
+            self._last_pressed_time: Dict[Key, float] = {}
+        elif self.dev == 'mouse':
+            self._log_file.write('mouse_type, timestamp, x, y, button, press, dx, dy\n')
 
     def _end_session(self):
         """
-        Finish logging and close the log files.
+        write meta info and close the log files.
         """
 
         self._log_file.close()
@@ -105,6 +101,73 @@ class KeyTrackerPrivate:
         # start writing session summary
         self._meta_file.write(
             f'{self.start_datetime}, {self.end_datetime}, {self.end_time - self.start_time}\n')
+
+    def start(self):
+        """
+        Starts tracking: open a meta info file and write down the column names. start the listener.
+        """
+
+        self.stopped = False
+        self._get_paths()
+        self._start_session()
+        if not os.path.exists(os.path.join(self.metadata_dir, self.start_date)):
+            os.mkdir(os.path.join(self.metadata_dir, self.start_date))
+        meta_file_path = os.path.join(self.metadata_dir, self.start_date, f'{self.dev}_meta_{self.start_datetime}.csv')
+        self._meta_file = open(meta_file_path, 'w+')
+        self._meta_file.write('session_start, session_end, session_duration\n')
+        self.listener.start()
+        self.listener.join()
+
+    def stop(self):
+        """
+        Stops tracking: close the meta info file and stop the listener.
+        """
+        self._end_session()
+        self._meta_file.close()
+        self.listener.stop()
+        self.stopped = True
+
+    def renew_session(self):
+        """
+        End current session and start a new one. To be used as a cron job.
+        """
+
+        print('\n###################')
+        print(f'NEW {self.dev} LOGGER SESSION ENTERED')
+        print('###################\n')
+
+        self._lock.acquire()  # to avoid collision with a key press or release
+
+        try:
+            self._end_session()
+            self._start_session()
+
+        finally:
+            self._lock.release()
+
+
+class KeyTrackerPrivate(TrackerBase):
+    """
+    A key tracker which identifies 'backspace' and 'delete' keys and groups
+    other keys into 'left_alphanum', 'right_alphanum', or 'other special' keys.
+    Attributes
+    ----------
+    _is_last_action_release : bool
+        Whether the last action is release or not
+    _last_pressed_key : pynput.keyboard.Key
+        The last key pressed
+    _last_pressed_time : float
+        The timestamp at which the last key was pressed at any given time
+        during session
+    """
+
+    def __init__(self):
+        # create output directory
+        super(KeyTrackerPrivate, self).__init__()
+        self.dev = 'key'
+        self.listener = keyboard.Listener(
+            on_press=self._on_press,
+            on_release=self._on_release)
 
     def _on_press(self, key: Key):
         """
@@ -117,7 +180,6 @@ class KeyTrackerPrivate:
         """
 
         self._lock.acquire()  # guarantee ongoing press/release actions complete
-
         try:
             # Only update last_pressed_time[key] if following a release action
             # or if the last key pressed is not the current key pressed. In other
@@ -175,54 +237,6 @@ class KeyTrackerPrivate:
 
         except Exception as e:
             print('Exception on release:', e)
-
-        finally:
-            self._lock.release()
-
-    def _upload_logs(self):
-        """
-        Upload the log files to designated cloud storage.
-        """
-        # subprocess.run(f'rclone copy {source} {target}')
-
-        raise NotImplementedError
-
-    def start(self):
-        """
-        Starts a session, which can be terminated by keyboard interrupt.
-        """
-
-        # time.sleep(1)
-        self.stopped = False
-        self._start_session()
-        if not os.path.exists(os.path.join(self.metadata_dir, self.start_date)):
-            os.mkdir(os.path.join(self.metadata_dir, self.start_date))
-        meta_file_path = os.path.join(self.metadata_dir, self.start_date, f'{self.dev}_meta_{self.start_datetime}.csv')
-        self._meta_file = open(meta_file_path, 'w+')
-        self._meta_file.write('session_start, session_end, session_duration\n')
-        self.listener.start()
-        self.listener.join()
-
-    def stop(self):
-        self._end_session()
-        self._meta_file.close()
-        self.listener.stop()
-        self.stopped = True
-
-    def renew_session(self):
-        """
-        End current session and start a new one. To be used as a cron job.
-        """
-
-        print('\n###################')
-        print('NEW KEY LOGGER SESSION ENTERED')
-        print('###################\n')
-
-        self._lock.acquire()  # to avoid collision with a key press or release
-
-        try:
-            self._end_session()
-            self._start_session()
 
         finally:
             self._lock.release()
